78289525b298450d15b22d063b011b899c482bea
kemeldev <kemel.developer@gmail.com>
2024-01-19 14:15:45 -0600

    Add project files.

---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonReviewApp.Models
+{
+    // Models o POCO stands for "Plain Old CLR Object." It is a design principle used in OOP where you create simple, lightweight classes that primarily serve as data containers without containing much behavior or logic. POCO classes are often used to represent data structures, entities, or models in an application.
+
+    // Para el caso de API los POCO son para representar datos de las bases de datos
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+    }
+}
---
+++ PokemonReviewApp/Models/PokemonCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    // Esta es una join table, porque si hacemos esto en las tablas existente tendriamos data redudante, es mejor crear tablas nuevas con informacion de varias tablas
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+        public int CategoryId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
+    }
+}
